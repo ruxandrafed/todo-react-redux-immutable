e6b7b20ac7c9ee954e229c9eec9f088cd263869b
ruxandrafed <ruxandra.fediuc@gmail.com>
2016-07-01 12:53:56 -0700

    Add actions and reducer

--- src/app.js
+++ src/app.js
@@ -2,14 +2,30 @@ import React from 'react';
 import { render } from 'react-dom';
 import { TodoList } from './components';
 
-const dummyTodos = [
-  { id: 0, isDone: true,  text: 'make components' },
-  { id: 1, isDone: false, text: 'design actions' },
-  { id: 2, isDone: false, text: 'implement reducer' },
-  { id: 3, isDone: false, text: 'connect components' }
-];
+// const dummyTodos = [
+//   { id: 0, isDone: true,  text: 'make components' },
+//   { id: 1, isDone: false, text: 'design actions' },
+//   { id: 2, isDone: false, text: 'implement reducer' },
+//   { id: 3, isDone: false, text: 'connect components' }
+// ];
+
+// render(
+//   <TodoList todos={dummyTodos} />,
+//   document.getElementById('app')
+// );
+
+import { List, Map } from 'immutable';
+
+const dummyTodos = List([
+  Map({ id: 0, isDone: true,  text: 'make components' }),
+  Map({ id: 1, isDone: false, text: 'design actions' }),
+  Map({ id: 2, isDone: false, text: 'implement reducer' }),
+  Map({ id: 3, isDone: false, text: 'connect components' })
+]);
+
+const store = createStore(reducer);
 
 render(
-  <TodoList todos={dummyTodos} />,
+  <TodoList todos={store.getState()} />,
   document.getElementById('app')
 );
